d886c5f79146aac753bfe1728d6e6b61172770a4
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-03-16 19:59:42 -0300

    30 days default

--- js/main.js
+++ js/main.js
@@ -2,25 +2,35 @@
 d3.csv('https://covid.ourworldindata.org/data/total_cases.csv')
 .then(makeChart);
 
+console.log(d3.csv('https://covid.ourworldindata.org/data/total_cases.csv'))
+
 function makeChart(cases) {
+  // all-time dates and case numbers
   var dateFull = cases.map(function(d) {return d.date});
   var brasilCasesFull = cases.map(function(d) {return d.Brazil});
-  var dateSeven = dateFull.slice(dateFull.length - 7, dateFull.length);
-  var brasilCasesSeven = brasilCasesFull.slice(brasilCasesFull.length - 7, brasilCasesFull.length);
+  // 30-day period 
+  var dateTrinta = dateFull.slice(dateFull.length - 30, dateFull.length);
+  var brasilCasesTrinta = brasilCasesFull.slice(brasilCasesFull.length - 30, brasilCasesFull.length);
+  // 7-day period
+  var dateSete = dateFull.slice(dateFull.length - 7, dateFull.length);
+  var brasilCasesSete = brasilCasesFull.slice(brasilCasesFull.length - 7, brasilCasesFull.length);
 
     var chart = new Chart('chart', {
       type: 'line',
       data: {
-        labels: dateFull,
+        labels: dateTrinta,
         datasets: [
           {
             label: 'Casos Confirmados no Brasil',
-            data: brasilCasesFull,
+            data: brasilCasesTrinta,
             backgroundColor: 'rgba(255, 200, 132, 0)',
             borderColor: 'rgba(50, 168, 82, 1)'
           },
         ],
-        layout: {
+        options: {
+          scales: {
+            display: false
+          }
         }
       }
     });
